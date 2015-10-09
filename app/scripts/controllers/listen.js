3782367833fd7d54d19a0e4765f338fce8cb59a4
kduckets <kmditroia@gmail.com>
2015-10-08 17:45:58 -0700

    convert blob to base64

--- app/scripts/controllers/listen.js
+++ app/scripts/controllers/listen.js
@@ -13,15 +13,48 @@ app.controller('ListenCtrl', function($scope, $window, $http, Audio){
     recordRTC.setRecordingDuration(5 * 1000).onRecordingStopped(function() {
        console.log('recording stopped');
         $scope.recordedBlob = recordRTC.getBlob();
+        
+        // converts blob to base64
+            var blobToBase64 = function(blob, cb) {
+             var reader = new FileReader();
+             reader.onload = function() {
+                 var dataUrl = reader.result;
+             var base64 = dataUrl.split(',')[1];
+                cb(base64);
+                 };
+                 reader.readAsDataURL($scope.recordedBlob);
+                };
+        blobToBase64($scope.recordedBlob, function(base64){ // encode
+            var base64blob = {'blob': base64};
+            console.log(base64blob);
+            
+         $http.post('/api/uploadtrack', base64blob)
+            .success(function(data) {
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+        });
+
+    });
+    recordRTC.startRecording();
+		}).catch(function(error) {
+   			 console.error(error);
+		});
+		};
 
 
-        $http({
+
+});
+
+           /*     $http({
                 method : 'POST',
                 headers: {
-   				'Content-Type': 'application/octet-stream',
-   				'Accept':'application/json'
- 				},
- 				
+                'Content-Type': 'application/octet-stream',
+                'Accept':'application/json'
+                },
+                
                 url : 'https://developer.echonest.com/api/v4/track/upload?api_key=LFJOHTELG1WX5SPLW&filetype=wav&track=' + $scope.recordedBlob
                
             }).success(function(data) {
@@ -29,13 +62,4 @@ app.controller('ListenCtrl', function($scope, $window, $http, Audio){
 
             }).error(function() {
                 console.log('POST /audio error'); 
-            });
-
-    })
-    recordRTC.startRecording();
-		}).catch(function(error) {
-   			 console.error(error);
-		});
-		};
-
-});
+            });*/
--- server.js
+++ server.js
@@ -8,18 +8,53 @@
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
+    
+
     // configuration =================
 
     //mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
 
     app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
+     var bodyParser = require('body-parser');
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+   
     app.use(methodOverride());
 
     // listen (start app with node server.js) ======================================
+
+    //routes
+      //upload audio track
+    app.post('/api/uploadtrack', function(req, resp){
+    
+      
+        console.log('body='+ req.body.blob);
+        var body = new Buffer(req.body.blob, 'base64'); // decode
+
+       
+      
+        if (!body)
+        {
+            return resp.status(500).send({error: 'bad parameters'})
+        }
+    
+
+        //initialize options values, the value of the method can be changed to POST to make https post calls
+        var apiKey = 'LFJOHTELG1WX5SPLW';
+        var options = {
+            host :  'developer.echonest.com',
+            port : 443,
+            path : '/api/v4/track/upload?api_key=' + apiKey + '&filetype=wav&track=' + body,
+            method : 'POST'
+        }
+
+            //TODO call the echonest API
+    });
+
   
     app.listen(8080);
     console.log("App listening on port 8080");
+
+  
