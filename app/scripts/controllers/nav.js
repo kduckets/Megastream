85d1bbdef329d14f2ee5b0825e7003151c0b48e1
kduckets <kmditroia@gmail.com>
2015-12-04 11:19:37 -0800

    cleanup

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -5,6 +5,7 @@ app.controller('NavCtrl', function ($scope, $location, Post, Auth, $http, $cooki
   $scope.signedIn = Auth.signedIn;
   $scope.logout = Auth.logout;
 
+//TODO: move this to angular route
  var qs = $location.search();
     if(qs.oauth_verifier){
 
@@ -16,7 +17,7 @@ app.controller('NavCtrl', function ($scope, $location, Post, Auth, $http, $cooki
 
     $http.post('/api/callback', body)
             .success(function(data) {
-              
+
 
     }, function (error) {
       $scope.error = error.toString();
